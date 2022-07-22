fc1df9d4feb397c75868330bf5564b5bc0b37c89
Choi-Ji-Yun <cjy5025@naver.com>
2022-07-22 20:15:22 +0900

    Fix: Account 관련 구문 오류 해결

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from 'src/account/account.entity';
 import { Category } from 'src/category/category.entity';
 import { StoreController } from './store.controller';
 import { Store } from './store.entity';
 import { StoreService } from './store.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Store, Category])],
+  imports: [TypeOrmModule.forFeature([Store, Category, Account])],
   controllers: [StoreController],
   providers: [StoreService],
   exports: [StoreService],
