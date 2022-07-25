ed99a02c043907784a0ea342d490e222a4b6d2db
Choi-Ji-Yun <cjy5025@naver.com>
2022-07-25 18:54:17 +0900

    Feat: 전체 주종 조회 API 추가 (#WTD-207)
    실시간 예약 페이지 로드 시 JWT 토큰 인증 후 전체 주종 목록 반환

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Category } from './category.entity';
+import { CategoryService } from './category.service';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  @Get()
+  @UseGuards(AuthGuard())
+  getAllCategories(): Promise<Category[]> {
+    return this.categoryService.getAllCategories();
+  }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './category.entity';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(@InjectRepository(Category) private categoryRepository: Repository<Category>) {}
+
+  async getAllCategories(): Promise<Category[]> {
+    return await this.categoryRepository.find({ cache: true });
+  }
+}
