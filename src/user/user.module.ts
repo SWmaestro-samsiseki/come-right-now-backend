6a5b934c83f3f3c83343defbfbcaff9936906f4e
junho100 <bemodesty306@naver.com>
2022-07-27 19:05:19 +0900

    Refactor: socket gateway 모듈 분리

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StoreModule } from 'src/store/store.module';
 import { UserController } from './user.controller';
 import { User } from './user.entity';
-import { UserGateway } from './user.gateway';
 import { UserService } from './user.service';
 
 @Module({
   imports: [StoreModule, TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService, UserGateway],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
