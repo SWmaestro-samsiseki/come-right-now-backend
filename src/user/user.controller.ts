912e3cb7bf42ff543e2ea5b4c6bc1a85d7fc95cd
Choi-Ji-Yun <cjy5025@naver.com>
2022-07-30 03:50:53 +0900

    Feat: user 정보 반환 API 추가
    - AuthGuard를 통한 사용자 인증
    - 인증된 유저 정보 반환

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { getAccount } from 'src/account/get-account.decorator';
+import { UserInfoDTO } from './user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('/info')
+  @UseGuards(AuthGuard())
+  getUserInfo(@getAccount() account): Promise<UserInfoDTO> {
+    const { id } = account;
+    return this.userService.getUserInfo(id);
+  }
+}
