551b993089561959722c64cc40f93e8a934cb0e1
junho100 <bemodesty306@naver.com>
2022-08-04 22:11:36 +0900

    Feat: POST /reservation-events/seat-request 수정
    수정된 api 문서 반영. api 문서 참고

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Store } from 'src/store/store.entity';
+import { User } from 'src/user/user.entity';
 import { ReservationController } from './reservation.controller';
 import { Reservation } from './reservation.entity';
 import { ReservationService } from './reservation.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reservation])],
+  imports: [TypeOrmModule.forFeature([Reservation, User, Store])],
   controllers: [ReservationController],
   providers: [ReservationService],
   exports: [ReservationService],
