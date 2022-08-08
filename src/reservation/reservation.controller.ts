e83dd6b0c64a5db064a28fb0403ff1f34be6f440
junho100 <bemodesty306@naver.com>
2022-08-09 02:58:27 +0900

    Fix: getUserReserved, getStoreReservedAndPending 수정
    api 문서 참고하여 리턴값 reservation으로 변경. dto 제거

--- src/reservation/reservation.controller.ts
+++ src/reservation/reservation.controller.ts
@@ -1,7 +1,5 @@
 import { Body, Controller, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 import { createReservationDTO } from './dto/create-reservation.dto';
-import { getStoreReservationDTO } from './dto/get-store-reservation.dto';
-import { getUserReservedDTO } from './dto/get-user-reserved.dto';
 import { Reservation } from './reservation.entity';
 import { ReservationService } from './reservation.service';
 
@@ -10,43 +8,23 @@ export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
 
   @Get('user/:id')
-  async getUserReserved(@Query('status') status: string, @Param('id') userId: string) {
+  async getUserReserved(
+    @Query('status') status: string,
+    @Param('id') userId: string,
+  ): Promise<Reservation> {
     const reservation = await this.reservationService.getReservationByUserId(status, userId);
-    const getUserReservedDTO: getUserReservedDTO = {
-      reservationId: reservation.id,
-      arrivalTime: reservation.arrivalTime,
-      numberOfPeople: reservation.peopleNumber,
-      storeId: reservation.store.id,
-    };
 
-    return getUserReservedDTO;
+    return reservation;
   }
 
   @Get('store/:id')
-  async getStoreReservedAndPending(@Query('status') status: string, @Param('id') storeId: string) {
+  async getStoreReservedAndPending(
+    @Query('status') status: string,
+    @Param('id') storeId: string,
+  ): Promise<Reservation[]> {
     const reservations = await this.reservationService.getStoreReservationByStatus(status, storeId);
-    const results: getStoreReservationDTO[] = [];
-    for (const reservation of reservations) {
-      const { estimatedTime } = reservation;
-      const reservationId = reservation.id;
-      const numberOfPeople = reservation.peopleNumber;
-      const userName = reservation.user.name;
-      const userPhone = reservation.user.phone;
-      const creditRate = reservation.user.creditRate;
 
-      const getStoreReservationDTO: getStoreReservationDTO = {
-        reservationId,
-        estimatedTime,
-        numberOfPeople,
-        userName,
-        userPhone,
-        creditRate,
-      };
-
-      results.push(getStoreReservationDTO);
-    }
-
-    return results;
+    return reservations;
   }
 
   @Post()
