db46b2a71ba50788e4dcd46dda0542fbdd0b6601
junho100 <bemodesty306@naver.com>
2022-08-10 22:34:02 +0900

    Fix: category 엔티티 수정

--- src/category/category.entity.ts
+++ src/category/category.entity.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Category extends BaseEntity {
   @PrimaryGeneratedColumn(/*'increment'*/)
+  @ApiProperty()
   public id: number;
 
   @Column({ type: 'varchar', length: 20, nullable: false })
+  @ApiProperty()
   public name: string;
 
   @Column({ type: 'varchar', length: 250, nullable: false })
+  @ApiProperty()
   public image: string;
 }
