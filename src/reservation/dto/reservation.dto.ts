257cb76438f295127f57c1fa4bb54f35f66fb84c
Choi-Ji-Yun <cjy5025@naver.com>
2022-10-01 17:27:27 +0900

    Refactor: ApiProperty 데코레이터 추가

--- src/reservation/dto/reservation.dto.ts
+++ src/reservation/dto/reservation.dto.ts
@@ -1,23 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { ReservationStatus } from 'src/enum/reservation-status.enum';
 import { StoreForPublicDTO } from 'src/store/dto/store-for-public.dto';
 import { User } from 'src/user/user.entity';
 
 export class ReservationDTO {
+  @ApiProperty()
   id: number;
 
+  @ApiProperty()
   numberOfPeople: number;
 
+  @ApiProperty()
   estimatedTime: Date;
 
+  @ApiProperty()
   createdAt: Date;
 
+  @ApiProperty()
   reservationStatus: ReservationStatus;
 
+  @ApiProperty()
   delayCount: number;
 
+  @ApiProperty()
   user: User;
 
+  @ApiProperty()
   store: StoreForPublicDTO;
 
+  @ApiProperty()
   departureTime: Date;
 }
