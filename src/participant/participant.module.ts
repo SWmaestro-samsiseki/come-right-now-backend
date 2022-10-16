ceb5e5e8329e7e725fe15c75dcd4a1cd051be5f8
junho100 <bemodesty306@naver.com>
2022-10-16 15:54:43 +0900

    Fix: 타임딜 신청 시 validation 추가

--- src/participant/participant.module.ts
+++ src/participant/participant.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TimeDeal } from 'src/time-deal/time-deal.entity';
+import { TimeDealModule } from 'src/time-deal/time-deal.module';
 import { User } from 'src/user/user.entity';
 import { ParticipantController } from './participant.controller';
 import { Participant } from './participant.entity';
@@ -11,6 +12,7 @@ import { ParticipantService } from './participant.service';
   imports: [
     TypeOrmModule.forFeature([Participant, TimeDeal, User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    forwardRef(() => TimeDealModule),
   ],
   controllers: [ParticipantController],
   providers: [ParticipantService],
--- src/time-deal/time-deal.module.ts
+++ src/time-deal/time-deal.module.ts
@@ -1,11 +1,9 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DateUtilModule } from 'src/date-util/date-util.module';
 import { LoggerModule } from 'src/logger/logger.module';
 import { ParticipantModule } from 'src/participant/participant.module';
-import { storeOnlineMap } from 'src/reservation-events/onlineMaps/store.onlineMap';
-import { ReservationEventsModule } from 'src/reservation-events/reservation-events.module';
 import { StoreModule } from 'src/store/store.module';
 import { TimeDealEventsGateway } from './time-deal-events.gateway';
 import { TimeDealController } from './time-deal.controller';
@@ -19,9 +17,10 @@ import { TimeDealService } from './time-deal.service';
     DateUtilModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     LoggerModule,
-    ParticipantModule,
+    forwardRef(() => ParticipantModule),
   ],
   controllers: [TimeDealController],
   providers: [TimeDealService, TimeDealEventsGateway],
+  exports: [TimeDealService],
 })
 export class TimeDealModule {}
