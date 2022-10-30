9e0e2744c369d1aac162602ff1fe12199cbdfbac
Choi-Ji-Yun <cjy5025@naver.com>
2022-10-31 02:03:42 +0900

    Fix: newrelic.ts 파일 경로 수정

--- newrelic.js
+++ newrelic.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 /**
  * New Relic agent configuration.
  *
@@ -20,7 +20,7 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: 'info'
+    level: 'info',
   },
   /**
    * When true, all request headers except for those listed in attributes.exclude
@@ -33,8 +33,8 @@ exports.config = {
       /**
        * Toggles whether the agent gathers log records for sending to New Relic.
        */
-      enabled: true
-    }
+      enabled: true,
+    },
   },
   attributes: {
     /**
@@ -55,7 +55,7 @@ exports.config = {
       'response.headers.authorization',
       'response.headers.proxyAuthorization',
       'response.headers.setCookie*',
-      'response.headers.x*'
-    ]
-  }
-}
+      'response.headers.x*',
+    ],
+  },
+};
